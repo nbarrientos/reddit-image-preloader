b7680b752a72f49d8507107450b27fdbd5af01f7
Nacho Barrientos <nacho@criptonita.com>
2010-10-12 23:00:28 +0200

    Use expando_button. SHOW/HIDE links

--- reddit_preloadimgs.js
+++ reddit_preloadimgs.js
@@ -20,26 +20,32 @@
     }
   }
 
-  function GM_rip_flip_visibility(expando, link) {
-    var displaying = link.data("showing");
-    displaying ? expando.hide() : expando.show();
-    link.data("showing", !displaying);
+  function GM_rip_flip_visibility(expando, button) {
+    var showing = button.data("showing");
+    showing ? expando.hide() : expando.show();
+    var text = showing ? "SHOW" : "HIDE";
+    button.text(text);
+    button.data("showing", !showing);
    }
 
   function GM_rip_img_onload(event) {
-    var link = $(event.data.link);
+    var entry = $(event.data.entry);
     var expando = $(event.data.expando);
-    link.data("showing", false);
-    var span = $("<span></span>").css("color", "red")
-                                 .css("font-weight", "bold")
-                                 .text("» ");
-    link.prepend(span);
+    var expando_button = $("<div></div>").addClass("expando-button")
+                              .css("width", "34px")
+                              .css("marginTop", "5px");
+    $("p.title", entry).after(expando_button);
+    var expando_button_anchor = $("<a href='javascript:void(0)'></a>")
+                              .data("showing", false)
+                              .css("color", "#336699")
+                              .text("SHOW")
+                              .appendTo(expando_button);
     $(this).show();
-    link.click(function() {
-        GM_rip_flip_visibility(expando, $(this));
+    expando_button_anchor.click(function() {
+        GM_rip_flip_visibility(expando, expando_button_anchor);
     });
     $(this).click(function() {
-        GM_rip_flip_visibility(expando, link);
+        GM_rip_flip_visibility(expando, expando_button_anchor);
     });
   }
 
@@ -49,12 +55,11 @@
         var href = link.attr("href");
         var pattern = /(\.jpg$|\.png$|\.gif$)/;
         if(pattern.test(href)) {
-            link.removeAttr("href");
-            var expando = $(".expando", $(this));
+            var expando = $(".expando", this);
             var inner = $("<div></div>").addClass("md");
             var img = $("<img/>").css("width", "100%")
                                  .css("overflow", "visible")
-                                 .bind("load", {link: link, expando: expando}, GM_rip_img_onload)
+                                 .bind("load", {entry: this, expando: expando}, GM_rip_img_onload)
                                  .attr("src", href);
             expando.children(".error").hide();
             inner.append(img);
